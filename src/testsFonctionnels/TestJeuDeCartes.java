2ac2b3f993808b36f9479b95c8093caedfe6c6a6
Timothe Viala <vlt4764a@ensfsi.onmicrosoft.com>
2025-03-06 09:24:17 +0100

    TP2, finir question 4

--- src/testsFonctionnels/TestJeuDeCartes.java
+++ src/testsFonctionnels/TestJeuDeCartes.java
@@ -1,18 +1,41 @@
 package testsFonctionnels;
 
 import cartes.JeuDeCartes;
+import utils.GestionCartes;
+
+import java.util.*;
+
+import cartes.*;
 
 public class TestJeuDeCartes {
 	public static void main(String[] args) {
-		JeuDeCartes jeu = new JeuDeCartes();
-		
-		jeu.initialiserJeu();
-		
-		System.out.println("JEU:\n" + jeu.affichageJeuCartes());
-		
-        /*if (!jeu.checkCount()) {
-            System.out.println("erreur de nombre");
-        }*/
-
+//		JeuDeCartes jeu1 = new JeuDeCartes();
+//		
+//		jeu1.initialiserJeu();
+//		
+//		System.out.println("JEU:\n" + jeu1.affichageJeuCartes());
+//		
+//		System.out.println("CheckCount du jeu: "+jeu1.checkCount());
+//		
+//        if (!jeu1.checkCount()) {
+//            System.out.println("erreur de nombre");
+//        }
+        
+        JeuDeCartes jeu = new JeuDeCartes();
+        jeu.initialiserJeu();
+        List<Carte> listeCarteNonMelangee = new LinkedList<>();
+        for (Carte carte : jeu.donnerCartes()) {
+        	listeCarteNonMelangee.add(carte);
+        }
+        List<Carte> listeCartes = new ArrayList<>(listeCarteNonMelangee);
+        System.out.println(listeCartes);
+        listeCartes = GestionCartes.melanger(listeCartes);
+        System.out.println(listeCartes);
+        System.out.println("liste mélangée sans erreur ? "
+        + GestionCartes.verifierMelange(listeCarteNonMelangee, listeCartes));
+        listeCartes = GestionCartes.rassembler(listeCartes);
+        System.out.println(listeCartes);
+        System.out.println("liste rassemblée sans erreur ? "
+        + GestionCartes.verifierRassemblement(listeCartes));
 	}
 }
